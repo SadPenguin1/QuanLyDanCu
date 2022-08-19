4765c6df491e7a65c97ca72c8f372a206044582a
ngnambka00-github <nam.nv182698@sis.hust.edu.vn>
2022-08-19 15:13:13 +0700

    first commit

---
+++ QLKhuPho/src/control/DBconnect.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DBconnect {
+    static Connection conn = null;
+
+    public DBconnect(String URL, String userName, String passWord) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(URL, userName, passWord);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public DBconnect() {
+        this("jdbc:sqlserver://localhost:1433;databaseName=DBKhuPho", "sa", "123456");
+    }
+//
+    public static ResultSet getData(String sql) {
+        ResultSet rs = null;
+        Statement state;
+        try {
+            state = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            rs = state.executeQuery(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        new DBconnect();
+    }
+}
